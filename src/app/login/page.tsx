e1be1cbdc8ebf11ea3e5ec90dd4036ee95ae828a
Maynor 125 <maynoldemar@gmail.com>
2024-01-11 21:15:13 -0600

    aplicando los estilos parte 1

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,15 +11,26 @@ import GoogleIcon from "../../../public/images/auth/google.png";
 import Image from "next/image";
 
 import TextField from "@mui/material/TextField";
-import { FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput } from "@mui/material";
+import {
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+} from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
+import Link from "next/link";
+
+import Divider from "@mui/material/Divider";
 
 const Login = () => {
   const [showPassword, setShowPassword] = React.useState(false);
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
-  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
     event.preventDefault();
   };
   return (
@@ -27,22 +38,24 @@ const Login = () => {
       <section className="container-login">
         <article className="section-a">
           <div className="logo-container">
-            <Image className="logo" alt="logotipo" src={Logo} />
+            <Link href="/">
+              <Image className="logo" alt="logotipo" src={Logo} />
+            </Link>
           </div>
           <div className="auth-container">
             <div className="auth-credential">
               <div className="auth-credential-head">
                 <h4>Login</h4>
-                <p>Daftar</p>
+                <Link href="/" className="link-head">Daftar</Link>
               </div>
-              <form action="">
+              <form action="" className="form">
                 <TextField
                   id="outlined-basic"
                   label="Digita tu email"
                   variant="outlined"
                   size="medium"
                 />
-                <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+                <FormControl sx={{ width: "100%" }} variant="outlined">
                   <InputLabel htmlFor="outlined-adornment-password">
                     Password
                   </InputLabel>
@@ -65,21 +78,22 @@ const Login = () => {
                     label="Password"
                   />
                 </FormControl>
-                <button className="boton-base btn-login">Login</button>
+                <button className="boton btn-login">Login</button>
               </form>
             </div>
             <div className="etiqueta">
+              <Divider sx={{ zIndex: -1 }} className="divider" />
               <p>Puedes registrarte con</p>
             </div>
             <div className="auth-more">
               <div className="cont-btns-more">
-                <button className="icon-btn">
-                   <Image alt="face-icon" src={FacebookIcon}/>
-                   Facebook
+                <button className=" boton icon-btn face-btn">
+                  <Image className="icon" alt="face-icon" src={FacebookIcon} />
+                  Facebook
                 </button>
-                <button className="icon-btn">
-                   <Image alt="google-icon" src={GoogleIcon}/>
-                   Facebook
+                <button className="boton icon-btn goo-btn">
+                  <Image className="icon" alt="google-icon" src={GoogleIcon} />
+                  Google
                 </button>
               </div>
             </div>
@@ -87,14 +101,22 @@ const Login = () => {
         </article>
         <article className="section-b">
           <div className="banner-container">
-            <Image alt="Decoration image" src={Decoration} />
+            <Image
+              alt="Decoration image"
+              src={Decoration}
+              className="img-decoration"
+            />
             <div className="container-morf">
               <div className="slider-object">
                 <h2>
                   Start your journey by one click, explore beautiful world!
                 </h2>
                 <div className="img-container">
-                  <Image alt="imagen productor" src={ImageBanner} />
+                  <Image
+                    className="imagen-productor"
+                    alt="imagen productor"
+                    src={ImageBanner}
+                  />
                 </div>
               </div>
             </div>
