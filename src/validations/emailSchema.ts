1170e2ede39a75ed89e118b87ff14ba79898a089
Maynor 125 <maynoldemar@gmail.com>
2024-01-15 14:49:42 -0600

    creacion del esquema de validacion de email

---
+++ src/validations/emailSchema.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+export const EmailSchema = z.object({
+  name: z
+    .string()
+    .min(10, {
+      message: "Name must be at least 10 characters long",
+    })
+    .max(100, {
+      message: "Name must be at least 200 characters long",
+    })
+    .refine((data) => !/^\s+$/.test(data), {
+      message: "La cadena no debe contener solo espacios en blanco",
+    }),
+  email: z.string().email(),
+  message: z
+    .string()
+    .min(20, {
+      message: "Message must be at least 10 characters long",
+    })
+    .max(400, {
+      message: "Message must be at least 400 characters long",
+    })
+    .refine((data) => !/^\s+$/.test(data), {
+      message: "La cadena no debe contener solo espacios en blanco",
+    }),
+});
