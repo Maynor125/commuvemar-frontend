2fcde9a558884a19ce59c3017f874812dfddb8b5
Maynor 125 <maynoldemar@gmail.com>
2024-01-18 17:16:50 -0600

    paleta elegida

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,3 +1,5 @@
+
+//paleta de colores a usar.
 export const colorTokens = {
     grey: {
         0: "#FFFFFF",
@@ -14,4 +16,63 @@ export const colorTokens = {
         900: "#0F0F15",
         1000: "#000000",
       },
+      primary:{
+        50: "#E6FBFF",
+        100: "#CCF7FE",
+        200: "#99EEFD",
+        300: "#00A2DC",
+        400: "#D3BEBE",
+        500: "#EE4623",
+        600: "#E83D21",
+        700: "#BEF9C1",
+        800: "#42C37D",
+        900: "#4FBD55",
+      }
+}
+
+//creacion del tema.
+export const themeSettings =(mode:string)=> {
+ return{
+    palette:{
+        mode:mode,
+        ...(mode === "dark"?{
+          // valores de la paleta para el dark mode
+          primary:{
+            dark:colorTokens.primary[300],
+           main:colorTokens.primary[600],
+           light:colorTokens.primary[900]
+           },
+           neutral: {
+            dark: colorTokens.grey[100],
+            main: colorTokens.grey[200],
+            mediumMain: colorTokens.grey[300],
+            medium: colorTokens.grey[400],
+            light: colorTokens.grey[700],
+          },
+          background: {
+            default: colorTokens.grey[900],
+            alt: colorTokens.grey[800],
+          },
+        }:
+        {
+          // palette values for light mode
+          primary: {
+            dark: colorTokens.primary[300],
+            main: colorTokens.primary[600],
+            light: colorTokens.primary[900],
+          },
+          neutral: {
+            dark: colorTokens.grey[700],
+            main: colorTokens.grey[500],
+            mediumMain: colorTokens.grey[400],
+            medium: colorTokens.grey[300],
+            light: colorTokens.grey[50],
+          },
+          background: {
+            default: colorTokens.grey[10],
+            alt: colorTokens.grey[0],
+          },
+        })
+    }
+ }
 }
