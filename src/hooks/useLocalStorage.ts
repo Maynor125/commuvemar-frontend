190fd7356f6b9b93fdde5c9c8092255ee951e432
Maynor 125 <maynoldemar@gmail.com>
2024-01-18 21:45:31 -0600

    creando la logica para guardar en local storage

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,17 @@
+// src/hooks/useLocalStorage.ts
+import { useState } from 'react';
+
+const useLocalStorage = <T>(key: string, initialValue: T) => {
+  const storedValue = localStorage.getItem(key);
+  const initial: T = storedValue ? JSON.parse(storedValue) : initialValue;
+  const [value, setValue] = useState<T>(initial);
+
+  const setStoredValue = (newValue: T) => {
+    setValue(newValue);
+    localStorage.setItem(key, JSON.stringify(newValue));
+  };
+
+  return [value, setStoredValue] as const;
+};
+
+export default useLocalStorage;
