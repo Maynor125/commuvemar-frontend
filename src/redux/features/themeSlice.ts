0b7a3fdff103c2d291ce06ef45fcb722c104604f
Maynor 125 <maynoldemar@gmail.com>
2024-01-22 09:49:51 -0600

    arreglando bug

--- src/redux/features/themeSlice.ts
+++ src/redux/features/themeSlice.ts
@@ -1,7 +1,7 @@
 "use client";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-// Obtén el estado inicial del tema desde localStorage
+/* Obtén el estado inicial del tema desde localStorage
 const loadDarkModeFromStorage = () => {
     const storedDarkMode = localStorage.getItem('darkMode');
     return storedDarkMode ? JSON.parse(storedDarkMode) : false;
@@ -13,6 +13,10 @@ const loadDarkModeFromStorage = () => {
 
 const initialState:ThemeState = {
   darkMode: loadDarkModeFromStorage(),
+};*/
+const storedDarkMode = typeof localStorage !== 'undefined' ? localStorage.getItem('darkMode') : null;
+const initialState = {
+  darkMode: storedDarkMode ? JSON.parse(storedDarkMode) : false,
 };
 
 export const themeSlice = createSlice({
