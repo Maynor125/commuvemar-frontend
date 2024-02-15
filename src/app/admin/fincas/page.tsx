716c4bf22493e5ff6bf90470e24aba9701838649
Maynor 125 <maynoldemar@gmail.com>
2024-02-15 10:36:39 -0600

    creacion del crud logico y la estructura

---
+++ src/app/admin/fincas/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Fincas = () => {
+  return (
+    <div>Fincas</div>
+  )
+}
+
+export default Fincas
---
+++ src/types/fincas.ts
@@ -0,0 +1,9 @@
+export interface Fincas {
+  id: number;
+  nombre: string;
+  comunidad: string;
+  areaCacaoProduccion: string;
+  areaCacaoDesarrollo: string;
+  produccionUltimoSiclo: string;
+  IDProductor: number;
+}
