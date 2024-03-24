979c47130d707d08934ece530ec0482275f1bb1e
Maynor 125 <maynoldemar@gmail.com>
2024-03-24 09:14:08 -0600

    creando modal de comprovacion

--- src/components/admin/profile/ProfilePreview.tsx
+++ src/components/admin/profile/ProfilePreview.tsx
@@ -27,6 +27,7 @@ import { getUser } from "@/utils/user";
 import { Workers } from "@/types/inspectors";
 import { useRouter } from "next/navigation";
 import { logout } from "@/redux/features/authSlice";
+import CustomDialog from "../ModalAlert";
 
 interface ProfileViewerProps {
   avatarSrc: string;
@@ -66,17 +67,18 @@ const ProfilePreview: React.FC<ProfileViewerProps> = ({ avatarSrc }) => {
     }
   }, [userState]);
 
-  const logOut=()=>{
+  const logOut = () => {
     dispatch(logout());
     localStorage.removeItem("token");
-    router?.push('/');
-  }
+    router?.push("/");
+    setOpenM(false);
+  };
 
   const [userInfo, setUserInfo] = useState<Workers | null>(null);
   const getInfoUser = async (id: number) => {
     try {
       const response = await getUser(id);
-      console.log("Info del usuario",response);
+      console.log("Info del usuario", response);
       if (response.data !== undefined) {
         setUserInfo(response.data);
       }
@@ -84,7 +86,6 @@ const ProfilePreview: React.FC<ProfileViewerProps> = ({ avatarSrc }) => {
       console.error(error);
     }
   };
-
 
   const handleOpenProfile = (event: React.MouseEvent<HTMLElement>) => {
     setOpenP(!openP);
@@ -114,14 +115,26 @@ const ProfilePreview: React.FC<ProfileViewerProps> = ({ avatarSrc }) => {
     height: "1.4rem",
   };
 
-  const [visible, setVisible] = useState(false);
+  const [openM, setOpenM] = React.useState(false);
+  console.log("la vaina desde la principal", openM);
+
+  const handleOpen = () => {
+    setOpenM(!openM);
+  };
+  const handleClose = () => {
+    setOpenM(!openM);
+  };
 
   return (
     <Box sx={containerStyles}>
       {" "}
       <Avatar src={avatarSrc} alt="User Avatar" />
       <IconButton
-        sx={{ color: theme.palette.secondary.dark,transition:'ease-in', transitionDuration:'.3s' }}
+        sx={{
+          color: theme.palette.secondary.dark,
+          transition: "ease-in",
+          transitionDuration: ".3s",
+        }}
         onClick={handleOpenProfile}
       >
         {!openP ? <KeyboardArrowDownIcon /> : <KeyboardArrowUpIcon />}
@@ -130,7 +143,8 @@ const ProfilePreview: React.FC<ProfileViewerProps> = ({ avatarSrc }) => {
         ref={containerRef}
         sx={{
           width: "20rem",
-
+          transition: "ease-in",
+          transitionDuration: ".6s",
           display: openP ? "block" : "none",
           position: "absolute",
           top: "2.9rem",
@@ -238,7 +252,10 @@ const ProfilePreview: React.FC<ProfileViewerProps> = ({ avatarSrc }) => {
               }}
             >
               <List>
-                <ListItemButton sx={{ textAlign: "inherit", height: "2.2rem" }}>
+                <ListItemButton
+                  onClick={handleOpen}
+                  sx={{ textAlign: "inherit", height: "2.2rem" }}
+                >
                   <ListItemText
                     sx={{
                       color: theme.palette.secondary.contrastText,
@@ -252,6 +269,38 @@ const ProfilePreview: React.FC<ProfileViewerProps> = ({ avatarSrc }) => {
           </Box>
         </div>
       </Box>
+      <CustomDialog
+        open={openM}
+        onAccept={logOut}
+        title="Advertencia"
+        description="Estas seguro de querer cerrar sesion?"
+        onClose={handleClose}
+        icon={
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="40"
+            height="40"
+            color="#E83D21"
+            fill="none"
+          >
+            <path
+              d="M7.02331 5.5C4.59826 7.11238 3 9.86954 3 13C3 17.9706 7.02944 22 12 22C16.9706 22 21 17.9706 21 13C21 9.86954 19.4017 7.11238 16.9767 5.5"
+              stroke="currentColor"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M12 2V10"
+              stroke="currentColor"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        }
+      />
     </Box>
   );
 };
