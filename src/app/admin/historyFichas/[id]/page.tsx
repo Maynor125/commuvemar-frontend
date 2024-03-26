3d321b6237d76eadf202b23e3fcc23e8537ed6cd
Maynor 125 <maynoldemar@gmail.com>
2024-03-25 20:39:00 -0600

    creacion inicial de la carta de la ficha

---
+++ src/app/admin/historyFichas/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Ficha = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default Ficha
--- src/app/admin/historyFichas/page.tsx
+++ src/app/admin/historyFichas/page.tsx
@@ -1,12 +1,40 @@
-import ProtectedPage from '@/middleware/ProtectedPage'
-import React from 'react'
+"use client";
+
+import CardFicha from "@/components/admin/fichas/CardFichas";
+import ProtectedPage from "@/middleware/ProtectedPage";
+import { Box, Typography, useTheme } from "@mui/material";
+import React from "react";
+
 
 const HistoryFichas = () => {
+  const theme = useTheme();
   return (
-    <ProtectedPage>
-          <div>HistoryFichas</div>
-    </ProtectedPage>
-  )
-}
+    <Box component="main">
+      <Box
+        sx={{
+          marginTop: "1rem",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginBottom: "1rem",
 
-export default HistoryFichas
+        }}
+      >
+        <Typography color={theme.palette.secondary.light} variant="h5">
+          Historial de fichas levantadas
+        </Typography>
+      </Box>
+      <Box sx={{
+        marginTop:'1.5rem',
+        display: "flex",
+        gap:'1rem',
+        paddingBottom: "1rem",
+      }}>
+        <CardFicha/>
+        <CardFicha/>
+      </Box>
+    </Box>
+  );
+};
+
+export default HistoryFichas;
