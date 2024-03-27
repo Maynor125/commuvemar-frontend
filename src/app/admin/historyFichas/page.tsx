8090d0fbfe58e7d3274db311db9ba6b48ec25df8
Maynor 125 <maynoldemar@gmail.com>
2024-03-27 12:45:40 -0600

    creando contenedor con paginacion para las fichas

--- src/app/admin/historyFichas/page.tsx
+++ src/app/admin/historyFichas/page.tsx
@@ -1,37 +1,44 @@
 "use client";
 
-import CardFicha from "@/components/admin/fichas/CardFichas";
+import CardFicha2 from "@/components/admin/fichas/CardFicha2";
 import ProtectedPage from "@/middleware/ProtectedPage";
-import { Box, Typography, useTheme } from "@mui/material";
-import React from "react";
+import { Box, Divider, Typography, useTheme } from "@mui/material";
+import React, { useState } from "react";
+import Pagination from "@/components/admin/pagination/Pagination";
+import { Fichas } from "@/data/admin/fichas";
+
+import './Style.css'
 
 
 const HistoryFichas = () => {
   const theme = useTheme();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [fichasPerPage] = useState(6);
+  const indexOfLastFicha = currentPage * fichasPerPage;
+  const indexOfFirstFicha = indexOfLastFicha - fichasPerPage;
+  const currentFichas = Fichas.slice(indexOfFirstFicha, indexOfLastFicha);
+
+  const paginate = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <Box component="main">
-      <Box
-        sx={{
-          marginTop: "1rem",
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginBottom: "1rem",
-
-        }}
-      >
-        <Typography color={theme.palette.secondary.light} variant="h5">
-          Historial de fichas levantadas
-        </Typography>
+      <Box sx={{width:'100%',background:theme.palette.background.paper,borderRadius:'6px',marginBottom:'1rem',marginTop:'1.5rem'}}>
+        <Box sx={{height:'4rem',display:'flex',justifyContent:'space-between',alignItems:'center',marginLeft:'2.5rem'}}>
+         <Typography sx={{color:theme.palette.secondary.light}} variant="h5">Historial de fichas</Typography>
+        </Box>
+        <Divider/>
+        <Box sx={{margin:'1.5rem',padding:'1rem',display:"flex",justifyContent:'space-between',flexWrap:'wrap',gap:'1.5rem'}}>
+          {
+            currentFichas.map((fichas)=>(
+                       <CardFicha2 key={fichas.id} ficha={fichas}/>
+            ))
+          }
+        </Box>
+        <Box sx={{ mt: '1rem', textAlign: 'center' }}>
+        <Pagination fichasPerPage={fichasPerPage} totalFichas={Fichas.length} paginate={paginate} />
       </Box>
-      <Box sx={{
-        marginTop:'1.5rem',
-        display: "flex",
-        gap:'1rem',
-        paddingBottom: "1rem",
-      }}>
-        <CardFicha/>
-        <CardFicha/>
       </Box>
     </Box>
   );
---
+++ src/data/admin/fichas.ts
@@ -0,0 +1,98 @@
+export const Fichas = [
+    {
+        id:1,
+        nombre:'Juan',
+        fecha:'10 de julio de 2023',
+        email:'juan@gmail.com',
+        finca:'La Valentina',
+        productor:'Raul Perez',
+        location:{
+            latitud:' 13.7177027',
+            longitud:' -84.7721508'
+        }
+    },
+    {
+        id:2,
+        nombre:'Yari',
+        fecha:'19 de enero de 2023',
+        email:'Yari@gmail.com',
+        finca:'La Callaita',
+        productor:'Pedro Castro',
+        location:{
+            latitud:' 13.7177027',
+            longitud:' -84.7721508'
+        }
+    },
+    {
+        id:3,
+        nombre:'Juan',
+        fecha:'10 de julio de 2023',
+        email:'juan@gmail.com',
+        finca:'La Valentina',
+        productor:'Raul Perez',
+        location:{
+            latitud:' 13.7177027',
+            longitud:' -84.7721508'
+        }
+    },
+    {
+        id:4,
+        nombre:'Yari',
+        fecha:'19 de enero de 2023',
+        email:'Yari@gmail.com',
+        finca:'La Callaita',
+        productor:'Pedro Castro',
+        location:{
+            latitud:' 13.7177027',
+            longitud:' -84.7721508'
+        }
+    },
+    {
+        id:5,
+        nombre:'Juan',
+        fecha:'10 de julio de 2023',
+        email:'juan@gmail.com',
+        finca:'La Valentina',
+        productor:'Raul Perez',
+        location:{
+            latitud:' 13.7177027',
+            longitud:' -84.7721508'
+        }
+    },
+    {
+        idID:6,
+        nombre:'Yari',
+        fecha:'19 de enero de 2023',
+        email:'Yari@gmail.com',
+        finca:'La Callaita',
+        productor:'Pedro Castro',
+        location:{
+            latitud:' 13.7177027',
+            longitud:' -84.7721508'
+        }
+    },
+    {
+        id:7,
+        nombre:'Juan',
+        fecha:'10 de julio de 2023',
+        email:'juan@gmail.com',
+        finca:'La Valentina',
+        productor:'Raul Perez',
+        location:{
+            latitud:' 13.7177027',
+            longitud:' -84.7721508'
+        }
+    },
+    {
+        id:8,
+        nombre:'Yari',
+        fecha:'19 de enero de 2023',
+        email:'Yari@gmail.com',
+        finca:'La Callaita',
+        productor:'Pedro Castro',
+        location:{
+            latitud:' 13.7177027',
+            longitud:' -84.7721508'
+        }
+    },
+]
