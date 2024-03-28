cccf20e6ae39372018e739cc35f8f58086509811
Maynor 125 <maynoldemar@gmail.com>
2024-03-28 14:17:36 -0600

    creando la pagina inicial donde se mirara la fica completa

--- src/components/admin/fichas/CardFicha2.tsx
+++ src/components/admin/fichas/CardFicha2.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import React, { FC } from "react";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Ficha } from "../../../types/ficha";
+import { useRouter } from "next/navigation";
 
 interface Props {
   onClick: ()=> void;
@@ -11,6 +12,11 @@ interface Props {
 
 const CardFicha2: FC<Props> = ({onClick,ficha}) => {
     const theme = useTheme();
+    const router = useRouter();  
+
+    const handleOpenFicha = () =>{
+      router.push(`/admin/historyFichas/${ficha.id}`)
+    }
 
   return (
     <div className="cardFicha" style={{background:theme.palette.background.default}}>
@@ -49,7 +55,7 @@ const CardFicha2: FC<Props> = ({onClick,ficha}) => {
         </div>
       </div>
       <div className="footerCard">
-        <Button sx={{ textTransform: "none" }} variant="outlined"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" color="#168cc8" fill="none" style={{ marginRight:'5px'}}>
+        <Button onClick={handleOpenFicha} sx={{ textTransform: "none" }} variant="outlined"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" color="#168cc8" fill="none" style={{ marginRight:'5px'}}>
     <path d="M18.5 16C19.0057 16.4915 21 17.7998 21 18.5M18.5 21C19.0057 20.5085 21 19.2002 21 18.5M21 18.5L13 18.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
     <path d="M11 22H10.7273C7.46607 22 5.83546 22 4.70307 21.2022C4.37862 20.9736 4.09058 20.7025 3.8477 20.3971C3 19.3313 3 17.7966 3 14.7273V12.1818C3 9.21865 3 7.73706 3.46894 6.55375C4.22281 4.65142 5.81714 3.15088 7.83836 2.44135C9.09563 2 10.6698 2 13.8182 2C15.6173 2 16.5168 2 17.2352 2.2522C18.3902 2.65765 19.3012 3.5151 19.732 4.60214C20 5.27832 20 6.12494 20 7.81818V13" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
     <path d="M3 12C3 10.1591 4.49238 8.66667 6.33333 8.66667C6.99912 8.66667 7.78404 8.78333 8.43137 8.60988C9.00652 8.45576 9.45576 8.00652 9.60988 7.43136C9.78333 6.78404 9.66667 5.99912 9.66667 5.33333C9.66667 3.49238 11.1591 2 13 2" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
