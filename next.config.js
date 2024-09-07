9a14e7bdde2bb2108a564184b7e0720f0ad28762
Maynor 125 <maynoldemar@gmail.com>
2024-09-06 18:22:23 -0600

    Corrigiendo errores de tipos en historyFichas

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
   images:{
     domains:["https://storage.googleapis.com"],
-  }
+  },
+  fonts: {
+    manifest: './fonts/manifest.json',
+    timeout: 30000,
+  },
 }
 
 module.exports = nextConfig
